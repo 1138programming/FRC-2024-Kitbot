0fde7cf69a0775151ffd72e0ec387bffd9e23409
KatiaAydin <kaydin26@chaminet.org>
2024-01-27 19:11:14 -0800

    switched motors controllers, base working

--- src/main/java/frc/robot/subsystems/CANDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CANDrivetrain.java
@@ -6,9 +6,10 @@ package frc.robot.subsystems;
 
 import static frc.robot.Constants.DrivetrainConstants.*;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /* This class declares the subsystem for the robot drivetrain if controllers are connected via CAN. Make sure to go to
@@ -23,39 +24,55 @@ public class CANDrivetrain extends SubsystemBase {
   different method calls. */
   DifferentialDrive m_drivetrain;
 
+  private VictorSPX leftFront;
+  private VictorSPX leftRear;
+  private VictorSPX rightFront;
+  private VictorSPX rightRear;
+
   /*Constructor. This method is called when an instance of the class is created. This should generally be used to set up
    * member variables and perform any configuration or set up necessary on hardware.
    */
   public CANDrivetrain() {
-    CANSparkMax leftFront = new CANSparkMax(kLeftFrontID, MotorType.kBrushed);
-    CANSparkMax leftRear = new CANSparkMax(kLeftRearID, MotorType.kBrushed);
-    CANSparkMax rightFront = new CANSparkMax(kRightFrontID, MotorType.kBrushed);
-    CANSparkMax rightRear = new CANSparkMax(kRightRearID, MotorType.kBrushed);
+    leftFront = new VictorSPX(kLeftFrontID);
+    leftRear = new VictorSPX(kLeftRearID);
+    rightFront = new VictorSPX(kRightFrontID);
+    rightRear = new VictorSPX(kRightRearID);
 
     /*Sets current limits for the drivetrain motors. This helps reduce the likelihood of wheel spin, reduces motor heating
      *at stall (Drivetrain pushing against something) and helps maintain battery voltage under heavy demand */
-    leftFront.setSmartCurrentLimit(kCurrentLimit);
-    leftRear.setSmartCurrentLimit(kCurrentLimit);
-    rightFront.setSmartCurrentLimit(kCurrentLimit);
-    rightRear.setSmartCurrentLimit(kCurrentLimit);
+    // leftFront.set .setSmartCurrentLimit(kCurrentLimit);
+    // leftRear.setSmartCurrentLimit(kCurrentLimit);
+    // rightFront.setSmartCurrentLimit(kCurrentLimit);
+    // rightRear.setSmartCurrentLimit(kCurrentLimit);
 
     // Set the rear motors to follow the front motors.
+    
     leftRear.follow(leftFront);
     rightRear.follow(rightFront);
 
     // Invert the left side so both side drive forward with positive motor outputs
-    leftFront.setInverted(true);
-    rightFront.setInverted(false);
+    leftFront.setInverted(false);
+    leftRear.setInverted(false);
+    rightFront.setInverted(true);
+    rightRear.setInverted(true);
 
     // Put the front motors into the differential drive object. This will control all 4 motors with
     // the rears set to follow the fronts
-    m_drivetrain = new DifferentialDrive(leftFront, rightFront);
+    // m_drivetrain = new DifferentialDrive(leftFront, rightFront);
   }
 
   /*Method to control the drivetrain using arcade drive. Arcade drive takes a speed in the X (forward/back) direction
    * and a rotation about the Z (turning the robot about it's center) and uses these to control the drivetrain motors */
   public void arcadeDrive(double speed, double rotation) {
-    m_drivetrain.arcadeDrive(speed, rotation);
+    // m_drivetrain.arcadeDrive(speed, rotation);
+    double leftSpeed = speed* .6 - rotation * .3;
+    double rightSpeed = speed* .6 + rotation * .3;
+
+    SmartDashboard.putNumber("rightSpeed", rightSpeed);
+    SmartDashboard.putNumber("leftSpeed", leftSpeed);
+
+    leftFront.set(ControlMode.PercentOutput, leftSpeed);
+    rightFront.set(ControlMode.PercentOutput, rightSpeed);
   }
 
   @Override
--- src/main/java/frc/robot/subsystems/CANLauncher.java
+++ src/main/java/frc/robot/subsystems/CANLauncher.java
@@ -6,22 +6,19 @@ package frc.robot.subsystems;
 
 import static frc.robot.Constants.LauncherConstants.*;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class CANLauncher extends SubsystemBase {
-  CANSparkMax m_launchWheel;
-  CANSparkMax m_feedWheel;
+  VictorSPX m_launchWheel;
+  VictorSPX m_feedWheel;
 
   /** Creates a new Launcher. */
   public CANLauncher() {
-    m_launchWheel = new CANSparkMax(kLauncherID, MotorType.kBrushed);
-    m_feedWheel = new CANSparkMax(kFeederID, MotorType.kBrushed);
-
-    m_launchWheel.setSmartCurrentLimit(kLauncherCurrentLimit);
-    m_feedWheel.setSmartCurrentLimit(kFeedCurrentLimit);
+    m_launchWheel = new VictorSPX(kLauncherID);
+    m_feedWheel = new VictorSPX(kFeederID);
   }
 
   /**
@@ -48,18 +45,18 @@ public class CANLauncher extends SubsystemBase {
 
   // An accessor method to set the speed (technically the output percentage) of the launch wheel
   public void setLaunchWheel(double speed) {
-    m_launchWheel.set(speed);
+    m_launchWheel.set(ControlMode.PercentOutput, speed);
   }
 
   // An accessor method to set the speed (technically the output percentage) of the feed wheel
   public void setFeedWheel(double speed) {
-    m_feedWheel.set(speed);
+    m_feedWheel.set(ControlMode.PercentOutput, speed);
   }
 
   // A helper method to stop both wheels. You could skip having a method like this and call the
   // individual accessors with speed = 0 instead
   public void stop() {
-    m_launchWheel.set(0);
-    m_feedWheel.set(0);
+    m_launchWheel.set(ControlMode.PercentOutput, 0);
+    m_feedWheel.set(ControlMode.PercentOutput, 0);
   }
 }
